d16674fb626fd5828025754774a5ea6d2cc3318c
RohitDayal <127755215+RohitDayal@users.noreply.github.com>
2023-11-26 14:35:56 +0530

    Update SignUpForm.jsx

--- client/src/components/SignUpForm/SignUpForm.jsx
+++ client/src/components/SignUpForm/SignUpForm.jsx
@@ -12,7 +12,7 @@ const SignUpForm = () => {
 
   async function register(ev) {
     ev.preventDefault();
-    const response = await fetch("http://localhost:5000/register", {
+    const response = await fetch("https://ease-dmsl.onrender.com/register", {
       method: "POST",
       body: JSON.stringify({ name, password, mail }),
       headers: { "Content-Type": "application/json" },
